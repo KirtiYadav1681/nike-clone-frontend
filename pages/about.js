c155053be17a39ba4ce8ce19260ace5f9b81ba28
KirtiYadav1681 <yadavkirti7745@gmail.com>
2024-01-09 20:03:21 +0530

    added contact us and about us page

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about-wrapper">
+      <div className="about"></div>
+      <div className="about-text">
+        <p>Our Mission</p>
+        <h1>BRING INSPIRATION AND INNOVATION TO <br/> EVERY ATHLETE* <br/> IN THE<br /> WORLD</h1>
+        <p style={{ fontStyle: "italic" }}>*If you have a body, you are an athlete</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div className="h-[47vh] bg-black text-white flex flex-col items-center justify-center">
+        <p>Contact us</p>
+        <p>Coming soon ... </p>
+    </div>
+  )
+}
+
+export default contact
